702d080d8baf18d0996616c790ffd7806dc9e0f3
ConrraD <WeSaySo@outlook.es>
2023-04-04 04:37:17 -0500

    termine el modulo 08

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -22,6 +22,7 @@ function operacionMatematica(num1, num2, cb) {
    // El callback realiza una operación matemática, por lo que necesita de los dos números.
    // Retorna el resultado del callback pasándole como parámetros los números.
    // Tu código:
+   cb(num1, num2);
 }
 
 function sumarArray(arrayOfNumbers, cb) {
@@ -30,6 +31,12 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+   let suma = 0;
+   for (i in arrayOfNumbers){
+      suma = suma + arrayOfNumbers[i];
+   }
+   cb(suma);
+
 }
 
 function forEach(array, cb) {
@@ -37,6 +44,9 @@ function forEach(array, cb) {
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
+    for (i in array){
+      cb(array[i]);
+    }
 }
 
 function map(array, cb) {
@@ -44,12 +54,25 @@ function map(array, cb) {
    // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
    // Retorna el nuevo arreglo.
    // Tu código:
+   nuevoArreglo = [];
+   for (i in array){
+      nuevoArreglo.push(cb(array[i]));
+   }
+   return nuevoArreglo;
+
 }
 
 function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+   let nuevoArreglo = [];
+   for (i in  arrayOfStrings){
+      if(arrayOfStrings[i][0] == 'a'){
+         nuevoArreglo.push(arrayOfStrings[i]);
+      }
+   }
+   return nuevoArreglo;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
